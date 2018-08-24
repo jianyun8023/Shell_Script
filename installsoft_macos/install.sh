f0a9347ecb15d2a35926dfa8180e57c882d69413
赵建云 <jianyun.zhao@zhaopin.com.cn>
2018-08-24 23:52:24 +0800

    修改注释

--- installsoft_macos/install.sh
+++ installsoft_macos/install.sh
@@ -19,14 +19,13 @@ installExtlib(){
 echo '安装常用的库'
 cat ./extlib.txt | while read line
 do
-    echo "$(brew list| grep -c $line)"
     if [ $(brew list| grep -c $line) -gt 0 ] ;
     then
          echo "软件 $line 已经安装,检查更新..."
          brew upgrade $line
     else
          echo "软件 $line 未安装,进行安装..."
-         #brew install $line
+         brew install $line
     fi
 done
 }
@@ -39,14 +38,13 @@ brew tap homebrew/cask-versions
 echo '安装常用软件'
 cat ./apps.txt | while read line
 do
-    echo "$(brew cask list| grep -c $line)"
     if [ $(brew cask list| grep -c $line) -gt 0 ] ;
     then
          echo "软件 $line 已经安装,检查更新..."
          brew cask upgrade $line
     else
          echo "软件 $line 未安装,进行安装..."
-         #brew cask install $line
+         brew cask install $line
     fi
 done
 }
